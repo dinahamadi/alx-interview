d5f79e9144f69cea8c068ec302a13322c2aa900a
dina <dinahamadi.20@gmail.com>
2024-10-18 01:55:43 +0100

    min operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This script defines the `minOperations` function which calculates the fewest number
+of operations required to achieve exactly `n` H characters in a text file, starting
+with a single 'H'.
+"""
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result in exactly n H characters.
+    """
+    if n <= 1:
+        return 0
+    operations = 0
+    factor = 2
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    return operations
