cd6394ae207debb53bb64c9fde4a1b39311e88d7
dina <dinahamadi.20@gmail.com>
2024-11-01 00:50:31 +0100

    task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+a script that determines if a given data set represents a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    num_bytes = 0
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
+    for byte in data:
+        byte &= 0xFF
+        if num_bytes == 0:
+            mask = 1 << 7
+            while mask & byte:
+                num_bytes += 1
+                mask >>= 1
+            if num_bytes == 0:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+        num_bytes -= 1
+    return num_bytes == 0
